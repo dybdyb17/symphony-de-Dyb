5903fa13c35c558267715219da5971eeea149d93
Boudiaf Dybril <dybrilboudiaf14@gmail.com>
2026-02-12 15:53:35 +0100

    autre modif du 12 fevrier

--- migrations/Version20260212142549.php
+++ migrations/Version20260212142549.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260212100201 extends AbstractMigration
+final class Version20260212142549 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20260212100201 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE product ADD image_filename VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD password VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE product DROP image_filename');
+        $this->addSql('DROP INDEX UNIQ_8D93D649E7927C74 ON user');
+        $this->addSql('ALTER TABLE user DROP password');
     }
 }
